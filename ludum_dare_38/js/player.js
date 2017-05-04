f8174051b1faa51567ce1deb67cc935fd5160a90
Frank <fwoodling@gmail.com>
2017-05-04 02:52:28 -0400

    rotation works, x/y calculations do not

--- ludum_dare_38/js/player.js
+++ ludum_dare_38/js/player.js
@@ -1,17 +1,19 @@
+car_image = new Image();
+car_image.src = './assets/cars/car_red_1.png';
+
 var key_pressed_a = false,
     key_pressed_d = false,
     key_pressed_w = false,
     key_pressed_s = false;
 
 var player_x = 50,
-    player_y = 50;
-    player_speed = 10;
-
+    player_y = 50,
+    player_speed = 10,
+    player_angle = 0,
+    max_turn_angle = 10; // probably needs to change based on speed
 
 document.addEventListener("keydown", function(e){
-  console.log(e.keyCode);
   switch(e.keyCode){
-
     case 87:
       key_pressed_w = true;
       break;
@@ -27,13 +29,11 @@ document.addEventListener("keydown", function(e){
     case 68:
       key_pressed_d = true;
       break;
-
   }
 }, false);
 
 document.addEventListener("keyup", function(e) {
   switch(e.keyCode) {
-
     case 87:
       key_pressed_w = false;
       break;
@@ -49,7 +49,6 @@ document.addEventListener("keyup", function(e) {
     case 68:
       key_pressed_d =false;
       break;
-
   }
 }, false);
 
@@ -60,39 +59,50 @@ window.requestAnimFrame = (function(callback) {
     };
 })();
 
-car_image = new Image();
-car_image.src = './assets/cars/car_red_1.png';
-
 var render_car = function(car_name, x, y, angle) {
   ctx.drawImage(car_image, x, y);
-  console.log('rendered')
 };
 
+function render_image(img, x, y, width, height, degrees) {
 
+  //Convert degrees to radian
+  var rad = degrees * Math.PI / 180;
 
+  //Set the origin to the center of the image
+  ctx.translate(x + width / 2, y + height / 2);
+
+  //Rotate the canvas around the origin
+  ctx.rotate(rad);
+
+  //draw the image
+  ctx.drawImage(img,width / 2 * (-1),height / 2 * (-1),width,height);
+
+  //reset the canvas
+  ctx.rotate(rad * ( -1 ) );
+  ctx.translate((x + width / 2) * (-1), (y + height / 2) * (-1));
+}
 
 function updateGame() {
 
-  // player_speed = 0;
-
   if(key_pressed_w) {
-    player_y -= player_speed
+    player_x += player_speed * Math.sin(player_angle);
+    player_y += player_speed * Math.cos(player_angle);
   }
   else if(key_pressed_s) {
-    player_y += player_speed
+    player_y += player_speed;
   }
   else if(key_pressed_a) {
-    player_x -= player_speed
+    player_angle -= max_turn_angle;
   }
   else if(key_pressed_d) {
-    player_x += player_speed
+    player_angle += max_turn_angle;
   }
 
-  // player_x += player_speed;
-  // player_y += player_speed;
+  console.log(player_angle);
+
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  render_car('a', player_x, player_y, 0);
+  render_image(car_image, player_x, player_y, car_image.width, car_image.height, player_angle);
 
   requestAnimFrame(function() {
     updateGame();
